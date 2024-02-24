9d01f9a8f8489a469ffed6f82672a4fa4e7b582f
ssandyy <ssandyy789@gmail.com>
2024-02-25 00:45:18 +0530

    test

--- SpringJdbcDemo/src/main/resources/data.sql
+++ SpringJdbcDemo/src/main/resources/data.sql
@@ -1,3 +1,4 @@
 insert INTO alien (id, name, technology) values(101, 'ssandyy', 'Javascript');
 insert INTO alien (id, name, technology) values(102, 'sundi', 'typescript');
 insert INTO alien (id, name, technology) values(103, 'mandy', 'Mern');
+insert INTO alien values(104, 'saty', 'python');
